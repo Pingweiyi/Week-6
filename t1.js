c82a735f7a7fe56cd6614e966ad94b739bcd544f
Pingweiyi <90896464+Pingweiyi@users.noreply.github.com>
2021-09-20 05:09:49 +0800

    Add files via upload

---
+++ t1.js
@@ -0,0 +1,32 @@
+
+	var aaa = new Vue({
+		el: '#aaa',
+		data:{
+		msg:"Please enter the user ID",
+		checked:"false",
+		picked:"Gender",
+		selected:"Month",
+	  }
+	});
+	
+	var bbb = new Vue({
+		el: '#bbb',
+		data:{
+		hobby:"CS:GO, LOL",
+
+	  }
+	});
+
+
+
+	Vue.createApp({
+	  data() {
+	    return {
+	      hobby:[]
+	    }
+	  }
+	}).mount('#bb')
+	
+
+
+	
